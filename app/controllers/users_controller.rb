6f4d7d1d42a08ed744c3a4da05533e52ba2e2ec0
EC2 Default User <ec2-user@ip-172-31-14-17.ap-northeast-1.compute.internal>
2025-10-08 19:40:36 +0000

    [Create]UserPage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,39 @@
 class UsersController < ApplicationController
   
   def show
-    @user = User.find(params[:id])
-    @user_images = @user.user_images
+    @user = User.find(current_user.id)
+    @posts = @user.posts
+    @post = Post.new
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
 
   def mypage
+    @user = User.find(current_user.id)
+    @posts = @user.posts
+    @post = Post.new
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] = "アップデートが完了しました"
+      redirect_to user_path(@user)
+    else
+      render :edit
+     end
   end
 
   private
     
   def user_params
-    params.require(:user).permit(:name, :user_image, :introduction)
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+    
+  def post_params
+    params.require(:post).permit(:title, :body, post_images: [])
   end
 
   def is_matching_login_user
