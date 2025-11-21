0906939af26aeb298910d0062b74e7a0254c1df0
EC2 Default User <ec2-user@ip-172-31-14-17.ap-northeast-1.compute.internal>
2025-11-21 22:59:39 +0000

    [Edit]seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,28 @@
+#管理者
 
-Admin.create(email: 'admin@example.com', 
-             password: 'password')
+  Admin.find_or_create_by!(email: 'admin@example.com') do |admin|
+    admin.password = 'password'
+  end
+
+#ユーザー
+
+  Alice = User.find_or_create_by!(email: "alice@example.com") do |user|
+    user.name = "Alice"
+    user.password = "passwordalice"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/alice_prf.jpg"), filename:"alice_prf.jpg")
+  end
+
+  Belle = User.find_or_create_by!(email: "belle@example.com") do |user|
+    user.name = "Belle"
+    user.password = "passwordbell"
+    user.profile_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/belle_prf.jpg"), filename:"belle_prf.jpg")
+  end
+
+#post
+
+  Post.find_or_create_by!(title: "七五三") do |post|
+    post_id = 1
+    post.post_image = ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/fixtures/shichigosan.jpeg"), filename:"shichigosan.jpeg")
+    post.body = "男の子なので三歳で七五三を行うか迷っていましたが、実際やってみてよかったです！被布は今だけなので三歳らしくてかわいい！"
+    post.user = Alice
+  end
